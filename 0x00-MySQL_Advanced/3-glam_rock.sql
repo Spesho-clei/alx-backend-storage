0c8d3ad523484e57dfbe6e908908ee607fe7c9a5
Spesho-clei <jepkoechnancy56@gmail.com>
2024-03-22 17:13:49 +0000

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,19 @@
 -- Task: List all bands with Glam rock as their main style, ranked by their longevity.
 
-SELECT band_name, 
-       IF(splitted[2] = 0, 2022 - formed[1], 2022 - formed[1] + (2022 - splitted[1])) AS lifespan
-FROM metal_bands
-WHERE main_style = 'Glam rock'
+SELECT band_name,
+       CASE
+           WHEN split IS NOT NULL THEN split - formed
+           ELSE 2022 - formed
+       END AS lifespan
+FROM (
+    SELECT band_name,
+           SUBSTRING_INDEX(lifespan, '-', 1) AS formed,
+           SUBSTRING_INDEX(lifespan, '-', -1) AS split
+    FROM metal_bands
+) AS band_lifespan
+WHERE band_name IN (
+    SELECT band_name
+    FROM metal_bands
+    WHERE main_style = 'Glam rock'
+)
 ORDER BY lifespan DESC;
